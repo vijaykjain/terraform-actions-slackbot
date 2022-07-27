8308b301e3923bac561786d95060802e5cac484b
vijaykjain <108300068+vijaykjain@users.noreply.github.com>
2022-07-26 22:50:16 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "aws_iam_role" "lambda_role" {
 
    assume_role_policy = <<EOF
 {
-  "Version": "2022-07-24",
+  "Version": "2012-10-17",
   "Statement": [
     {
       "Action": "sts:AssumeRole",
