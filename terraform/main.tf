32434b8a5154ce12416d3b83476d0853ea1a5590
vijaykjain <108300068+vijaykjain@users.noreply.github.com>
2022-07-26 21:51:29 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+    required_providers {
+      aws = {
+          source = "hashicorp/aws"
+          version = "~> 4.13.0"
+      }
+    }
+}
 provider "aws" {
     region = "us-east-1"
 }
